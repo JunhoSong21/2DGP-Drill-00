667c23c245fbfe317c4686ec9565ba8bddf59f68
JunhoSong21 <junhoasd1zxc2@gmail.com>
2024-09-23 13:32:05 +0900

    Mov

---
+++ Circle.py
@@ -0,0 +1,6 @@
+import turtle
+turtle.penup()
+turtle.goto(200, 200)
+turtle.pendown()
+turtle.circle(100)
+turtle.write('(200, 200)')
---
+++ name.py
@@ -0,0 +1,81 @@
+import turtle
+turtle.penup()
+turtle.goto(-250, 300)
+turtle.pendown()
+turtle.goto(-290, 260)
+turtle.goto(-250, 300)
+turtle.goto(-210, 260)
+turtle.penup()
+turtle.goto(-250, 260)
+turtle.pendown()
+turtle.goto(-250, 240)
+turtle.goto(-290, 240)
+turtle.goto(-210, 240)
+turtle.penup()
+turtle.goto(-250, 180)
+turtle.pendown()
+turtle.circle(20)
+turtle.penup()
+turtle.goto(-190, 300)
+turtle.pendown()
+turtle.goto(-110, 300)
+turtle.goto(-150, 300)
+turtle.goto(-190, 260)
+turtle.goto(-150, 300)
+turtle.goto(-110, 260)
+turtle.penup()
+turtle.goto(-110, 240)
+turtle.pendown()
+turtle.goto(-190, 240)
+turtle.goto(-150, 240)
+turtle.goto(-150, 220)
+turtle.penup()
+turtle.goto(-190, 220)
+turtle.pendown()
+turtle.goto(-190, 180)
+turtle.goto(-110, 180)
+turtle.penup()
+turtle.goto(-60, 300)
+turtle.pendown()
+turtle.goto(-40, 300)
+turtle.penup()
+turtle.goto(-90, 290)
+turtle.pendown()
+turtle.goto(-10, 290)
+turtle.penup()
+turtle.goto(-50, 220)
+turtle.pendown()
+turtle.circle(25)
+turtle.penup()
+turtle.goto(-50, 210)
+turtle.pendown()
+turtle.goto(-50, 180)
+turtle.goto(-90, 180)
+turtle.goto(-10, 180)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
